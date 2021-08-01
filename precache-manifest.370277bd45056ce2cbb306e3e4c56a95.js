e8e49d14676d7e032e170e31efc96b48bcd4f239
francisconeves97 <francisco.neves@tecnico.ulisboa.pt>
2021-08-01 22:43:25 +0100

    Updates

---
+++ precache-manifest.370277bd45056ce2cbb306e3e4c56a95.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5151cf95fd817988ec24115f5c15374d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "5922ed011ca308a99c50",
+    "url": "./static/css/main.42cc8b22.chunk.css"
+  },
+  {
+    "revision": "81d6419c0667b367bc27",
+    "url": "./static/js/2.28df21f2.chunk.js"
+  },
+  {
+    "revision": "9b162339341ee717a536a87392f598a5",
+    "url": "./static/js/2.28df21f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5922ed011ca308a99c50",
+    "url": "./static/js/main.a19f7fae.chunk.js"
+  },
+  {
+    "revision": "886067f5697c62f46402",
+    "url": "./static/js/runtime-main.b9e38f12.js"
+  }
+]);
