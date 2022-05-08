e77f9f9852bfa7a5a54e8f9cc33bead3c9ebd698
francisconeves97 <francisco.neves@tecnico.ulisboa.pt>
2022-05-08 09:19:51 +0100

    Fix parsing of forms with special characters

---
+++ src/scripts/__mocks__/mockParsedSpecialCharacterForm.ts
@@ -0,0 +1,46 @@
+export const mockParsedSpecialCharacterForm = `
+Object {
+  "action": "e/1FAIpQLSeT9rPzmpnaPRPrj6VXv9hXn-AYKYARfzstZmqIQJrkNYtxnQ",
+  "description": null,
+  "fbzx": "6170924624432982469",
+  "fields": Array [
+    Object {
+      "description": null,
+      "id": "1403784388",
+      "label": "Hello, what's up? ",
+      "options": Array [
+        Object {
+          "custom": false,
+          "label": "Option 1",
+        },
+        Object {
+          "custom": true,
+          "label": "",
+        },
+      ],
+      "required": false,
+      "type": "RADIO",
+    },
+    Object {
+      "description": null,
+      "id": "162741379",
+      "label": "Hello, what's up 2",
+      "options": Array [
+        Object {
+          "custom": false,
+          "label": "Option 1",
+        },
+      ],
+      "required": false,
+      "type": "RADIO",
+    },
+  ],
+  "fieldsOrder": Object {
+    "1403784388": 0,
+    "162741379": 1,
+  },
+  "fvv": 1,
+  "pageHistory": 0,
+  "title": null,
+}
+`
